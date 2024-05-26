57ccc14770bc63c2552f435b421a282e156eb5d9
jquinn99002 <james.q@outlook.com>
2024-05-26 17:27:23 +0100

    Update data.tf

--- Modules/azurerm_api_connection/data.tf
+++ Modules/azurerm_api_connection/data.tf
@@ -1,4 +1,4 @@
 data "azurerm_managed_api" "managed_api" {
-  name     = var.managed_api_name
-  location = var.location
+      name     = var.managed_api_name
+  location              = var.location
 }
